759494791f55b88478839fb94539023d8046dbc9
JollyRoker <jolly.roker@hotmail.it>
2018-08-02 20:16:24 +0200

    started editing

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
-const Alexa = require('ask-sdk-core');
+const Alexa = require('ask-sdk');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+const SKILL_NAME = 'Il Signore degli Enigmi';
+const WELCOME_MESSAGE = "Il signore degli enigmi ti da il benvenuto.";
+const RIDDLE_REQUEST = "L'indovinello che devi risolvere è: ";
+const HELP_MESSAGE = 'Puoi chiedere un indovinello, se non riesci a risolverlo puoi chiedere un indizio al giorno. Che posso fare per te?';
+const HELP_REPROMPT = 'Che posso fare per te?';
+const STOP_MESSAGE = 'Alla prossima!';
+const INFO_REQUEST = 'Puoi dire ripeti per riascoltare la citazione, o dire voto per dare una tua valutazione.';
+const ADDITIONAL_INFO = 'L\'autore della citazione è: ';
+const FALLBACK_MESSAGE = 'Questa skill non può soddisfare la tua richiesta. Può ispirarti grazie a delle citazioni quando la apri. Cosa posso fare per te?';
+const FALLBACK_REPROMPT = 'Che cosa posso fare per te?';
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
+//=========================================================================================================================================
+
+const data = [
+    {
+        riddle: "La mia vita può durare qualche ora, quello che produco mi divora. Sottile sono veloce, grossa sono lenta e il vento molto mi spaventa. Chi sono?",
+        answer: ["candela","la candela"]
+    }
+]
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
