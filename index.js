7b25394eceb68ba9f3c8eb83e4c6a7f3042bb198
JollyRoker <jolly.roker@hotmail.it>
2018-08-05 17:03:47 +0200

    Added AnswerRequest and AnswerIntent

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ const SKILL_NAME = 'Il Signore degli Enigmi';
 const WELCOME_MESSAGE = "Il signore degli enigmi ti da il benvenuto. Metti alla prova le tue capacità con indovinelli e rompicapi e spingi la tua mente agli estremi. Chiedi un nuovo indovinello o riprendi da dove hai lasciato.";
 const WELCOME_REPROMPT = "Chiedi un nuovo indovinello o riprendi da dove hai lasciato.";
 const RIDDLE_REQUEST = "L'indovinello che devi risolvere è:<break time='0.9s'/>";
+const INFO_ANSWER = "Puoi rispondere direttamente ora all'indovinello, o chiedere al signore degli enigmi di rispondere se ti verrà in mente più tardi.";
+const ANSWER_REQUEST1 = "L'indovinello a che stai cercando di risolvere è:<break time='0.9s'/> ";
+const ANSWER_REQUEST2 = "<break time='0.9s'/>Qual'è la tua risposta?";
+const RIGHT_ANSWER = "La risposta è corretta. Complimenti!";
+const WRONG_ANSWER = "Mi dispiace. La risposta è sbagliata";
 const HELP_MESSAGE = 'Puoi chiedere un indovinello, se non riesci a risolverlo puoi chiedere un indizio al giorno. Che posso fare per te?';
 const HELP_REPROMPT = 'Che posso fare per te?';
 const STOP_MESSAGE = 'Alla prossima!';
-const INFO_REQUEST = 'Puoi dire ripeti per riascoltare la citazione, o dire voto per dare una tua valutazione.';
 const ADDITIONAL_INFO = 'L\'autore della citazione è: ';
 const FALLBACK_MESSAGE = 'Questa skill non può soddisfare la tua richiesta. Può ispirarti grazie a delle citazioni quando la apri. Cosa posso fare per te?';
 const FALLBACK_REPROMPT = 'Che cosa posso fare per te?';
@@ -25,12 +29,12 @@ const FALLBACK_REPROMPT = 'Che cosa posso fare per te?';
 
 const data = [
     {
-        riddle: "La mia vita può durare qualche ora, quello che produco mi divora. Sottile sono veloce, grossa sono lenta e il vento molto mi spaventa. Chi sono?",
-        answer: ["candela","la candela"]
+        riddle: "La mia vita può durare qualche ora, quello che produco mi divora. Sottile sono veloce, grossa sono lenta, e il vento molto mi spaventa. Chi sono?",
+        answer: "candela"
     },
     {
         riddle: "Questo è un indovinello di prova",
-        answer: ["candela", "la candela"]
+        answer: "test"
     }
 ]
 
@@ -51,6 +55,7 @@ const LaunchRequestHandler = {
 
 const NewRiddleIntentHandler = {
     canHandle(handlerInput) {
+        console.log("Inside NewRiddleIntentHandler");
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'NewRiddleRequest';
     },
@@ -60,7 +65,7 @@ const NewRiddleIntentHandler = {
         const response = handlerInput.responseBuilder;
 
         if (attributes.current === undefined) {
-            console.log("This is a new riddle");
+            console.log("Looking for a new riddle");
             console.log(attributes.current);
 
             var riddle = getRiddle(handlerInput);
@@ -68,21 +73,75 @@ const NewRiddleIntentHandler = {
                     
             return response
                 .speak(speechText)
-                .withShouldEndSession(false)
+                .reprompt(INFO_ANSWER)
                 .withSimpleCard('Il signore degli enigmi', riddle)
                 .getResponse();
         } else {
-            console.log("This is an old riddle");
-            console.log(attributes.current);
+            console.log("Repeating the current riddle");
             const riddle = data[attributes.current].riddle;
             const speechText = RIDDLE_REQUEST + riddle;
 
             return response
                 .speak(speechText)
-                .withShouldEndSession(false)
+                .reprompt(INFO_ANSWER)
                 .withSimpleCard('Il signore degli enigmi', riddle)
                 .getResponse();
         }    
+    },
+};
+
+const AnswerRiddleRequestHandler = {
+    canHandle(handlerInput) {
+        console.log("Inside AnswerRiddleRequestHandler");
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+
+        return attributes.current !== undefined
+            && handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AnswerRiddleRequest';
+    },
+    handle(handlerInput) {
+        console.log("Inside AnswerRiddleRequest");
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        const riddle = data[attributes.current].riddle;
+        const speechText = ANSWER_REQUEST1 + riddle + ANSWER_REQUEST2;
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(ANSWER_REQUEST2)
+            .withSimpleCard('Il signore degli enigmi', speechText)
+            .getResponse();
+    },
+};
+
+const AnswerRiddleIntentHandler = {
+    canHandle(handlerInput) {
+        console.log("Inside AnswerRiddleIntentHandler");
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        return attributes.current !== undefined
+            && handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AnswerRiddleIntent';
+    },
+    handle(handlerInput) {
+        console.log("Inside AnswerRiddleIntent");
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        const answer = data[attributes.current].answer;
+        const isCorrect = isTheRightAnswer(handlerInput.requestEnvelope.request.intent.slots, answer);
+
+        if (isCorrect) {
+            const speechText = RIGHT_ANSWER;
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard('Il signore degli enigmi', speechText)
+                .getResponse();
+        } else {
+            const speechText = WRONG_ANSWER;
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard('Il signore degli enigmi', speechText)
+                .getResponse();
+        }
     },
 };
 
@@ -165,6 +224,18 @@ function getRiddle(handlerInput) {
     return randomRiddle;
 }
 
+function isTheRightAnswer(slots, value) {
+    for (const slot in slots) {
+        if (Object.prototype.hasOwnProperty.call(slots, slot) && slots[slot].value !== undefined) {
+            if (slots[slot].value.toString().toLowerCase() === value.toString().toLowerCase()) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
@@ -173,7 +244,9 @@ exports.handler = skillBuilder
         NewRiddleIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
-        SessionEndedRequestHandler
+        SessionEndedRequestHandler,
+        AnswerRiddleRequestHandler,
+        AnswerRiddleIntentHandler
     )
     .addErrorHandlers(ErrorHandler)
     .lambda();
